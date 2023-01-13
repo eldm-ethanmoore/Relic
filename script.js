1b2755cfa37e27864467cddab3795fac44bf1e0d
Ethan L. Moore <86084798+eldm-ethanmoore@users.noreply.github.com>
2023-01-13 21:39:17 +0000

    Added Tag.

--- script.js
+++ script.js
@@ -3,6 +3,11 @@ if(window.location.href === "https://www.gracefuljourney.xyz/" ||
    window.location.href === "https://gracefuljourney.xyz/")
 {
   console.log("GJ.com!")
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-24QFXMF42Y');
 } else {
   console.log("Development...")
 }
