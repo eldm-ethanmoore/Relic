1241153b39771127d46d8a25347a0fd42b26e531
Ethan L. Moore <86084798+eldm-ethanmoore@users.noreply.github.com>
2023-01-13 21:29:49 +0000

    Script.js added window.location.href check

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log(window.location.href);
+if(window.location.href === "https://www.gracefuljourney.xyz/" ||
+   window.location.href === "https://gracefuljourney.xyz/")
+{
+  console.log("GJ.com!")
+} else {
+  console.log("Development...")
+}
