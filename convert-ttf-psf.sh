4a2d47c66ddeaa38ee7643ba931e5b2b3a7630fb
eldm <eldm2350@protonmail.com>
2023-09-07 14:38:01 +0000

    Create convert-ttf-psf.sh

---
+++ convert-ttf-psf.sh
@@ -0,0 +1,2 @@
+otf2bdf -p 20 -r 96 -o Relic-Final-Normal.bdf Relic-Final-Normal.ttf
+bdf2psf --fb Relic-Final-Normal.bdf /usr/share/bdf2psf/standard.equivalents /usr/share/bdf2psf/fontsets/Uni2.512 512 Relic-Final-Normal.psf
